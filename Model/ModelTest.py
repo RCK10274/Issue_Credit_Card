ef666af3c9c11fe7b171e5ea913dd105606f247a
OAO <rck72165722@gmail.com>
2024-03-19 05:30:35 +0800

    20240319

--- Model/ModelTest.py
+++ Model/ModelTest.py
@@ -10,8 +10,6 @@ from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 
-
-
 def Table(df):
 
     df['LogIncome'] = np.log1p(df['income'])
@@ -41,71 +39,86 @@ import numpy as np
 
 
 class LogisticR:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, weight=None, bias=None, learning_rate=0.01, iter=100):
+        self.weight=weight
+        self.bias=bias
+        self.learning_rate=learning_rate
+        self.iter=iter
 
-    def sigmoid(self,z):#定義邏輯回函數
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        if self.weight==None:
+            self.weight = np.ones(X.shape[1])
+        if self.bias==None:
+            self.bias = 0
+
+    def sigmoid(self,z):#定義邏輯回函數(閥0.5)
         return 1 / (1 + np.exp(-z))#g(z) = 1/(1+e^-z)
 
-    def predict(self, features, weights, bias):
+    def predict(self, features):
         """
         features: 特徵
-        weights: 權重
-        bias: 偏至
         """
-        z = np.dot(features, weights) + bias#(Θ0 + Θ1*w1 + Θ2*w2 + Θn*wn)
-        return self.sigmoid(z)#回傳邏輯回歸假設函數
+        z = np.dot(features, self.weights) + self.bias#(Θ0 + Θ1*w1 + Θ2*w2 + Θn*wn)
+        return np.where(self.sigmoid(z)>=0.5, 1, 0)#回傳邏輯回歸假設函數
 
-    def compute_loss(self,true, pred):#計算損失函數
+    def loss_funtion(self,true, pred):#Cost(hΘ(x),y)=-ylog(hΘ(x)-(1-y)log(1-hΘ(x)))
         m = len(true)
-        loss = -(1/m) * np.sum(true * np.log(pred) + (1 - true) * np.log(1 - pred))
+        loss = np.sum(true * np.log(pred) - (1 - true) * np.log(1 - pred))
+        loss2 = -np.sum()#-Σ{1~i}Σ{1~c} yiclog(pre)
         return loss
 
-    # 定义梯度下降函数
-    def gradient_descent(self,features, y_true, weights, bias, learning_rate):
+    def cost_funtion(self,true, pred):#計算代價函數
+        return
+
+    def gradient_descent(self):
         """
-        features: 特徵
-        y_true: 真實預測值
         weights: 權重
         bias: 偏至
         learning_rate: 學習速率
         """
-        m = len(y_true)
-        y_pred = self.predict(features, weights, bias)
-        
-        dw = (1/m) * np.dot(features.T, (y_pred - y_true))
-        db = (1/m) * np.sum(y_pred - y_true)
-        
-        weights -= learning_rate * dw
-        bias -= learning_rate * db
-        
-        return weights, bias
+        m = len(self.y)
+        features = self.X
+        weights = self.weight
+        bias = self.bias
+
+        for _ in range(self.iter):
+            y_pred = self.predict(features, weights, bias)
+
+            #損失函數的差異
+            w = (1/m) * np.dot(features.T, (y_pred - y_true))#1/m Σ{1~m} x(hΘ(x)-y)
+            b = (1/m) * np.sum(y_pred - y_true)
+            #更新權重與偏移
+            weights -= self.learning_rate * w#Θ = Θ-learning_rate()
+            bias -= self.learning_rate * b
+            print(weights, bias)
+            
+        return weights,bias
+     
+    def score(self, X, y):
+        y_pred = self.predict(X)
+        accuracy = np.sum(y == y_pred) / len(y)
+        return accuracy
 
 
 
 
 data_numeric = pd.read_csv("Data/Data2.csv")
 data_numeric = data_numeric.astype(float)
-
-# 初始化权重和偏置
-np.random.seed(0)  # 确保可复现性
-weights = np.random.rand(data_numeric.shape[1] - 1)  # 不包括目标变量
-bias = np.random.rand()
-# 演示更新一次权重和偏置
 features = data_numeric.drop('card', axis=1).values
 y_true = data_numeric['card'].values
-learning_rate = 0.01
 
-# 更新权重和偏置
-weights, bias = gradient_descent(data_numeric.iloc[:,1:], data_numeric.iloc[:,0], weights, bias, learning_rate)
 
-# 输出更新后的权重和偏置
-
-#print(weights , bias)
-    
-    
+#z = features
+#a = 1 / (1 + np.exp(-z))
+#a = np.where(a<0.5, 0, 1)
+#print(np.sum(a==0))
+#print(np.sum(a!=0))
+'''
 X_train,  X_test, y_train, y_test = train_test_split(data_numeric.iloc[:,1:], data_numeric.iloc[:,0], test_size=0.2)
 
 LG = LogisticRegression()
 LG.fit(X_train,y_train)
 print(LG.score(X_test,y_test))
+'''
